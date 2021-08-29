263f31db47ff0c42603ba9b43e69d84ea68d9bcd
Ruargh Steyn <ruargh.steyn@protectz.co.uk>
2021-08-29 22:42:31 +0200

    task: validation error messages

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -6,7 +6,19 @@
     <form action="{{ route('messages.store') }}" method="POST">
         @csrf
         <div><input type="text" name="user_handle"></div>
+        @error('user_handle')
+            <div>{{ $message }}</div>
+        @enderror
         <div><textarea name="content"></textarea></div>
+        @if($errors->any())
+            <div>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <div><input type="submit" value="Create"></div>
     </form>
 @endsection
