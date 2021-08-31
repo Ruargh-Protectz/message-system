c9f01ac48edfa75de7d46e940bc2d958944eb4d8
Ruargh Steyn <ruargh.steyn@protectz.co.uk>
2021-08-31 22:47:34 +0200

    task: update views with authenticated user / guest

--- resources/views/messages/partials/form.blade.php
+++ resources/views/messages/partials/form.blade.php
@@ -1,7 +1,18 @@
 <div class="">
-    <label for="user_handle">User Handle</label>
-    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-    id="usr_handle" type="text" name="user_handle" value="{{ old('user_handle', optional($message ?? null)->user_handle) }}"></div>
+    <label for="user_handle">User</label>
+
+    @guest
+
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="usr_handle" type="text" name="user_handle" value="Anonymous" readonly="readonly" ></div>
+
+    @else
+
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        id="usr_handle" type="text" name="user_handle" value="{{ Auth::user()->name }}" readonly="readonly" ></div>
+
+    @endguest
+
 @error('user_handle')
     <div>{{ $message }}</div>
 @enderror
@@ -18,3 +29,5 @@
         </ul>
     </div>
 @endif
+
+
