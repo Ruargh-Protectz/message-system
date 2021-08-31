b36cb1614acd63a42faf1df3a0bf8c2e2f06d558
Ruargh Steyn <ruargh.steyn@protectz.co.uk>
2021-08-31 22:25:58 +0200

    task:  styles

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -3,11 +3,9 @@
 @section('title', 'Create message')
 
 @section('content')
-
-
     <form action="{{ route('messages.store') }}" method="POST">
         @csrf
-        <div class="form-group">
+        <div class="">
             <label for="user_handle">User Handle</label>
             <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="usr_handle" type="text" name="user_handle" value="{{ old('user_handle') }}"></div>
