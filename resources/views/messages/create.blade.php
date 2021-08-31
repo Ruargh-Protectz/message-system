6b0113fd40a6b5077515d1d8c9b1715836061ef7
Ruargh Steyn <ruargh.steyn@protectz.co.uk>
2021-08-31 08:39:53 +0200

    test: TailwindCSS styles

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -3,13 +3,21 @@
 @section('title', 'Create message')
 
 @section('content')
+
+
     <form action="{{ route('messages.store') }}" method="POST">
         @csrf
-        <div><input type="text" name="user_handle" value="{{ old('user_handle') }}"></div>
+        <div class="form-group">
+            <label for="user_handle">User Handle</label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="usr_handle" type="text" name="user_handle" value="{{ old('user_handle') }}"></div>
         @error('user_handle')
             <div>{{ $message }}</div>
         @enderror
-        <div><textarea name="content">{{ old('content') }}</textarea></div>
+        <div>
+            <label for="content">Message Content</label>
+            <textarea class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="content" name="content">{{ old('content') }}</textarea></div>
         @if($errors->any())
             <div>
                 <ul>
